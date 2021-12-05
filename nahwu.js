d8cc2993930ae6ce4b934929de94164bb0c1020e
Khathetheuh-ema <95558291+Khathetheuh-ema@users.noreply.github.com>
2021-12-05 15:02:55 +0700

    menambah fungsi help

--- nahwu.js
+++ nahwu.js
@@ -1,4 +1,19 @@
+//daftar fungsi
+const fungsi = [
+            "About()",
+       "Help()"
+      ]
+      
+const Kfungsi = [
+            "alert tentang aplikasi, versi dan kontak pengembang",
+       "alert bantuan menampilkan fungsi ataw featur yang tersedia"
+      ]
+   
 Function About(){
 alert("Kumpulan rumus-rumus dan kaidah dalam ilmu bahasa arab (ilmu nahwu) \n
       Version : 001.00 create by bebegig118 || 081220206315 || Bandung")
+}
+function Help(){
+      let alr = fungsi.map(function(e, i){return(e + " => " + Kfungsi[i])});
+      alert(alr.join("\n"));
 }
